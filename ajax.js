479fc1ff2fb356001abc5c981c10e8ea4d43dbe1
Jos van Olst <vanolstj@gmail.com>
2017-10-12 22:56:09 +1300

    Got map bounds working

--- ajax.js
+++ ajax.js
@@ -1,5 +1,10 @@
 $(document).ready(function() {
+    setInterval(updateMap, 30000);
     $("#route_list").change(function () {
+        updateMap();
+    });
+    function updateMap() {
+        var markers = []
         route = {route_short_name: $("#route_list").val()};
         $.post("get_vehicle_info.php", route, function (data) {
             vehicles = data;
@@ -9,12 +14,12 @@ $(document).ready(function() {
                 var point = new google.maps.LatLng(
                     parseFloat(markerElem[1]),
                     parseFloat(markerElem[2]));
+
                 var infowincontent = document.createElement('div');
                 var strong = document.createElement('strong');
                 strong.textContent = id
                 infowincontent.appendChild(strong);
                 infowincontent.appendChild(document.createElement('br'));
-
                 var start_time = document.createElement('text');
                 start_time.textContent = 'Start Time: ' + markerElem[3];
                 infowincontent.appendChild(start_time);
@@ -22,15 +27,26 @@ $(document).ready(function() {
                 var timestamp = document.createElement('text');
                 timestamp.textContent = 'Timestamp: ' + markerElem[4];
                 infowincontent.appendChild(timestamp);
+
                 var marker = new google.maps.Marker({
                     map: map,
                     position: point
                 });
-                marker.addListener('click', function() {
+                markers.push(marker);
+                marker.addListener('click', function () {
                     infoWindow.setContent(infowincontent);
                     infoWindow.open(map, marker);
                 });
+                var bounds = new google.maps.LatLngBounds();
+                for (var i = 0; i < markers.length; i++)
+                {
+                    bounds.extend(markers[i].getPosition());
+                }
+                map.fitBounds(bounds);
             });
         });
-    });
+    };
+    function clearMarkers() {
+        setMapOnAll(null);
+    };
 });
